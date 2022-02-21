0abf613322a28e11e06557e80c7e1fd84a51580c
AnastasijaBalaeva <balaeva9anastasija@gmail.com>
2022-02-22 00:21:12 +0300

    Initial myProjectShop

---
+++ src/main/java/com/shop/myProjectShop/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.shop.myProjectShop.repository;
+
+import com.shop.myProjectShop.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
---
+++ src/main/java/com/shop/myProjectShop/service/SecurityService.java
@@ -0,0 +1,4 @@
+package com.shop.myProjectShop.service;
+
+public class SecurityService {
+}
